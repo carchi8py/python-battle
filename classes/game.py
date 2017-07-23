65453f85f11648e5679ee913343d21ead7a95f58
Chris Archibald <carchi8py@gmail.com>
2017-07-23 11:41:38 -0700

    created main game

--- classes/game.py
+++ classes/game.py
@@ -25,3 +25,7 @@ class Person:
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
 
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from classes.game import Person, Bcolors
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 60},
+         {"name": "Thunder", "cost": 10, "dmg": 80},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
+print(player.generate_damage())
+print(player.generate_damage())
+print(player.generate_damage())
+print(player.generate_spell_damage(1))
+print(player.generate_spell_damage(2))
