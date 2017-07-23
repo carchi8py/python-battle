2cdea3a29fa7279b46c315fa6a2f73f9e78965fe
Chris Archibald <carchi8py@gmail.com>
2017-07-23 11:34:17 -0700

    Added main class

---
+++ main.py
@@ -0,0 +1,7 @@
+from classes.game import Person, Bcolors
+
+magic = [{"name": "Fire", "cost": 10, "dmg": 60},
+         {"name": "Thunder", "cost": 10, "dmg": 60},
+         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+
+player = Person(460, 65, 60, 34, magic)
