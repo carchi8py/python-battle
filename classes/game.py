010b622c35a1d28299d4b4ba4601ee64b3174624
Chris Archibald <carchi8py@gmail.com>
2017-07-23 11:19:28 -0700

    Set up generate damage

--- classes/game.py
+++ classes/game.py
@@ -1,3 +1,5 @@
+import random
+
 class Bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -19,3 +21,8 @@ class Person:
         self.df = df
         self.magic = magic
         self.actions = ["Attack", "Magic"]
+
+    def generate_damage(self):
+        return random.randrange(self.atkl, self.atkh)
+
+    
