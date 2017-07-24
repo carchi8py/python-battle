1c558b902881a83930b088d2fcc83ce186f94585
Chris Archibald <carchi8py@gmail.com>
2017-07-24 16:28:28 -0700

    Add basic game loop

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ magic = [{"name": "Fire", "cost": 10, "dmg": 60},
          {"name": "Blizzard", "cost": 10, "dmg": 60}]
 
 player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+
+print(Bcolors.FAIL + Bcolors.BOLD + "AN ENEMY ATTACKS!" + Bcolors.ENDC)
+
+while running:
+    print("========================")
+    player.choose_action()
+    choice = input("Choose action:")
+    index = int(choice) - 1
+
+    #if we chose attack the enemy need to take damage
+    if index == 0:
+        dmg = player.generate_damage()
+        enemy.take_damage(dmg)
+        print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+
+    enemy_choice = 1
+    enemy_dmg = enemy.generate_damage()
+    player.take_damage(enemy_dmg)
+    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp()11)
