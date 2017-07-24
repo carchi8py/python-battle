80620d774632be47f7dc69d3ef22d9b326f2611b
Chris Archibald <carchi8py@gmail.com>
2017-07-24 16:48:26 -0700

    Adding magic attacks

--- classes/game.py
+++ classes/game.py
@@ -68,5 +68,5 @@ class Person:
         i = 1
         print("Magic")
         for spell in self.magic:
-            print(str(i) + ":", spell["name"], "(cost:", str(spell["mp"]) + ")")
+            print(str(i) + ":", spell["name"], "(cost:", str(spell["cost"]) + ")")
             i += 1
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from classes.game import Person, Bcolors
 
-magic = [{"name": "Fire", "cost": 10, "dmg": 60},
-         {"name": "Thunder", "cost": 10, "dmg": 80},
-         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+magic = [{"name": "Fire", "cost": 10, "dmg": 100},
+         {"name": "Thunder", "cost": 12, "dmg": 124},
+         {"name": "Blizzard", "cost": 10, "dmg": 100}]
 
 player = Person(460, 65, 60, 34, magic)
 enemy = Person(1200, 65, 45, 25, magic)
@@ -14,19 +14,38 @@ print(Bcolors.FAIL + Bcolors.BOLD + "AN ENEMY ATTACKS!" + Bcolors.ENDC)
 while running:
     print("========================")
     player.choose_action()
-    choice = input("Choose action:")
-    index = int(choice) - 1
+    choice = int(input("Choose action:")) - 1
 
-    #if we chose attack the enemy need to take damage
-    if index == 0:
+    #if this is 0 we are doing a melee attack
+    if choice == 0:
         dmg = player.generate_damage()
         enemy.take_damage(dmg)
-        print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+        print("You attacked for", dmg, "points of damage.")
+
+    #if this is 1 we are doing magic attack
+    elif choice == 1:
+        player.choose_magic()
+        magic_choice = int(input("Choose magic:")) - 1
+        magic_dmg = player.generate_spell_damage(magic_choice)
+        spell = player.get_spell_name(magic_choice)
+        cost = player.get_spell_mp_cost(magic_choice)
+        current_mp = player.get_mp()
+
+        if cost > current_mp:
+            print(Bcolors.FAIL + "\nNot enough MP\n" + Bcolors.ENDC)
+            continue
+        player.reduce_mp(cost)
+        enemy.take_damage(magic_dmg)
+        print(Bcolors.OKGREEN + "\n" + spell + " deals", str(magic_dmg), "points of damage" + Bcolors.ENDC)
 
     enemy_choice = 1
     enemy_dmg = enemy.generate_damage()
     player.take_damage(enemy_dmg)
-    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
+    print("Enemy attacks for", enemy_dmg)
+    print("-----------------------------")
+    print("Enemy HP:", Bcolors.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + Bcolors.ENDC + "\n")
+    print("Your HP:", Bcolors.OKGREEN + str(player.get_hp()) + "/" + str(player.get_max_hp()) + Bcolors.ENDC)
+    print("Your MP:", Bcolors.OKBLUE + str(player.get_mp()) + "/" + str(player.get_max_mp()) + Bcolors.ENDC + "\n")
 
     #check if someone has died
     if enemy.get_hp() == 0:
